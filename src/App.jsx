401a8cee38b00e27598949a14e550fe728e04e96
devHksingh <hksingh.dev@gmail.com>
2024-07-19 13:10:45 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='mt-6'>
+        <h1 className='text-center font-semibold capitalize text-4xl'>Supabase</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
