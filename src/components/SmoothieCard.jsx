2197470c71d249bbdde4eb975c8494a35f695250
devHksingh <hksingh.dev@gmail.com>
2024-07-20 23:14:09 +0530

    Fetching Single Records

--- src/components/SmoothieCard.jsx
+++ src/components/SmoothieCard.jsx
@@ -1,4 +1,5 @@
-
+import { Link } from "react-router-dom";
+import { Pencil } from 'lucide-react';
 
 function SmoothieCard({ smoothie }) {
     console.log('smoothie :',smoothie);
@@ -7,6 +8,11 @@ function SmoothieCard({ smoothie }) {
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={'/'+smoothie.id}>
+          <Pencil />
+        </Link>
+      </div>
     </div>
   )
 }
