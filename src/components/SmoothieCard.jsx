fac853c7971207b8323c8fd5684aad51b38fd7be
devHksingh <hksingh.dev@gmail.com>
2024-07-21 17:13:18 +0530

    Deleting Records

--- src/components/SmoothieCard.jsx
+++ src/components/SmoothieCard.jsx
@@ -1,17 +1,36 @@
 import { Link } from "react-router-dom";
-import { Pencil } from 'lucide-react';
+import { Pencil,Trash } from 'lucide-react';
+import supabase from '../config/supabaseClient'
 
 function SmoothieCard({ smoothie }) {
+  
     console.log('smoothie :',smoothie);
+    const handleDelete = async()=>{
+      const {data,error}= await supabase
+         .from('smoothies')
+         .delete()
+         .eq('id',smoothie.id)
+         .select()
+
+    if(error){
+      console.log(error);
+    }
+    if(data){
+      console.log('deleted data',data);
+
+    }
+
+    }
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
-      <div className="buttons">
+      <div className="buttons flex font-light gap-4 mt-4">
         <Link to={'/'+smoothie.id}>
-          <Pencil />
+          <Pencil className="text-blue-600 cursor-pointer"/>
         </Link>
+        <Trash className="text-red-600 cursor-pointer" onClick={handleDelete}/>
       </div>
     </div>
   )
